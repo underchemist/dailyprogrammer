459fdef821796d34ce3be4d5f302545f6660f5a6
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2014-08-25 16:45:07 -0700

    first attempt

---
+++ 82514c177_quicksort/quicksort.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.4
+import random
+
+def compare(pivot, value):
+        if value < pivot:
+                return True
+        else:
+                return False
+
+def sort(L):
+
+if __name__=='__main__':
+        
+N = 20
+L = [3,5,7,2,5,8,10,23,3,1,7,6,42,14,93,2,28,66,20,11] 
